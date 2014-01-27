5263f8f5f75e9701ba7c0454a0532340c3b80204
Sébastien Maccagnoni-Munch <sebastien@tiramiseb.fr>
2014-01-27 16:53:35 +0100

    Improve the plugins skeleton

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,4 +1,5 @@
 # Default modules
+import configparser
 import logging
 import threading
 
@@ -7,16 +8,43 @@ import zmq
 
 ################################################################################
 
-class Plugin:
+class StopPlugin(Exception):
+    pass
 
+################################################################################
+
+class Plugin:
     def __init__(self, conf, section, *args, **kwargs):
-        self.conf = conf
-        self.sectionname = section
-        self.thread = threading.Thread(target=self.loop)
+        self.configparser = conf
+        self.section = section
+        self.thread = threading.Thread(target=self.__loop)
 
     def run(self):
         self.thread.start()
 
+    def __loop(self):
+        try:
+            self.loop()
+        except StopPlugin:
+            logging.error(
+                'Stopping plugin from section [{}] because of a '
+                'previous error'.format(self.section)
+            )
+
+    def conf(self, key):
+        try:
+            return self.configparser.get(self.section, key)
+        except configparser.NoOptionError:
+            logging.error(
+                'No option "{}" found in section [{}]'.format(
+                    key, self.section
+                )
+            )
+            raise StopPlugin
+
+    def error(self, msg):
+        logging.error(msg)
+        raise StopPlugin
 
 
 class SenderPlugin(Plugin):
@@ -38,12 +66,13 @@ class ReceiverPlugin(Plugin):
     messagefilter = ('',)
     def __init__(self, *args, **kwargs):
         Plugin.__init__(self, *args, **kwargs)
-        self.insock = kwargs['zmq_context'].socket(zmq.SUB)
+        self.insock = zmq.Context.instance().socket(zmq.SUB)
         for filter_ in self.messagefilters:
             self.insock.setsockopt_string(zmq.SUBSCRIBE, filter_)
         self.insock.connect("inproc://toplugin")
 
     def loop(self):
+        self.irsend('a')
         while True:
             self.receive(self.insock.recv_string())
 
