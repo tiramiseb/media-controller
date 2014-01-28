23dd4e333658a31ffdc9dda5ad11db72b2404f8c
Sébastien Maccagnoni-Munch <sebastien@tiramiseb.fr>
2014-01-28 14:51:29 +0100

    Squeezebox plugin

---
+++ plugins/squeezebox.py
@@ -0,0 +1,51 @@
+"""
+Configuration keys:
+
+* squeezelitepath: The path to the squeezelite executable
+* name: Name of the player
+
+This plugin understands the following messages:
+
+* squeezebox:start
+* squeezebox:stop
+"""
+
+# Default modules
+import logging
+import subprocess
+
+# Dependencies
+import psutil
+
+# Local modules
+from plugins import ReceiverPlugin
+
+################################################################################
+
+class Squeezebox(ReceiverPlugin):
+    messagefilters = ('squeezebox:')
+
+    def __init__(self, *args, **kwargs):
+        ReceiverPlugin.__init__(self, *args, **kwargs)
+        self.sqpath = self.conf('squeezelitepath')
+        self.sqname = self.conf('name')
+
+    def receive(self, msg):
+        if msg == 'squeezebox:start':
+            self.start()
+        elif msg == 'squeezebox:stop':
+            self.stop()
+
+    def start(self):
+        subprocess.call((
+            self.sqpath,
+            '-n',
+            self.sqname,
+            '-z'
+        ))
+
+    def stop(self):
+        procname = self.sqpath.split('/')[-1]
+        for proc in psutil.process_iter():
+            if proc.name == procname:
+                proc.kill()
